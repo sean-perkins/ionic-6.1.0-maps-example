5141bd3a055c4386252f1fc0bf887cfc43675d68
Sean Perkins <sean@ionic.io>
2022-04-06 16:48:42 -0400

    chore(): example concept for maps

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Keyboard, KeyboardResize } from '@capacitor/keyboard';
+import { IonModal, Platform } from '@ionic/angular';
 
+const API_KEY = 'AIzaSyAjZSqzUvriCqo1cs7XfVm8boAVq9ONGmI';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
 
-  constructor() {}
+  @ViewChild(IonModal) modal: IonModal;
+
+  mapSrc = this.sanitizer.bypassSecurityTrustResourceUrl(
+    `https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=Space+Needle,Seattle+WA`
+  );
+
+  constructor(private platform: Platform, private sanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+    if (this.platform.is('capacitor')) {
+      Keyboard.setResizeMode({
+        mode: KeyboardResize.None
+      });
+      Keyboard.setAccessoryBarVisible({ isVisible: true });
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.platform.is('capacitor')) {
+      Keyboard.setResizeMode({
+        mode: KeyboardResize.Ionic
+      });
+    }
+  }
+
+  onSearchbarFocused() {
+    // When the search bar is focused, set the modal breakpoint to 1
+    this.modal.setCurrentBreakpoint(1);
+  }
 
 }
